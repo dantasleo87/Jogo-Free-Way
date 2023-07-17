32c6b22b7578c1333eca9cd0ad2b94c8fd9ef06c
Léo Dantas <137342193+dantasleo87@users.noreply.github.com>
2023-07-17 04:27:25 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,58 @@
+//ator
+let xAtor = 85;
+let yAtor = 366;
+let colisao = false;
+let comprimentoCarro = 50
+let alturaCarro = 40
+let meusPontos = 0;
+
+function mostraAtor (){
+   image(imagemDoAtor,xAtor,yAtor,30,30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown (UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown (DOWN_ARROW)){
+    if (podeSeMover()){
+    yAtor += 3;
+    }
+  }
+}function verificaColisao (){
+  for (let i = 0;i < imagemCarros.length; i = i + 1){
+  colisao = collideRectCircle(xCarros[i], yCarros [i],comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
+        if (colisao){
+        voltaAtorParaPosicaoInicial();
+          somDaColisao.play();
+          if (pontosMaiorQueZero()){
+          meusPontos -= 1;
+      }
+    }          
+  }
+}
+function  voltaAtorParaPosicaoInicial (){
+  yAtor = 366;
+}
+
+function incluiPontos (){
+  fill (255,240,60)
+  textAlign (CENTER)
+  textSize (25);
+  text(meusPontos, width/5,27);
+}
+
+function marcaPonto (){
+  if (yAtor <15){
+    meusPontos += 1;
+    somDoPonto.play ();
+   voltaAtorParaPosicaoInicial ();
+}
+}
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover (){
+  return yAtor <366;
+}
